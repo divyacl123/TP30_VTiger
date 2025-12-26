857b2c6993d07834d392b4a354103cc4c9f619ec
Srinivas V <Srinivas V@LAPTOP-IH9RI1RB>
2025-12-26 16:25:38 +0530

    Updated the Branch name

---
+++ src/test/java/lead_module/LeadOfferingTest.java
@@ -0,0 +1,10 @@
+package lead_module;
+
+import org.testng.annotations.Test;
+
+public class LeadOfferingTest {
+@Test
+public void leading() {
+	System.out.println("--leading the organization--");
+}
+}
