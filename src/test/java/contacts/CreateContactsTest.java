30493ff73d6bdf5f636b5266fb91bc9644b3c305
Likitha H E <likithahe456@gmail.com>
2025-12-27 12:18:08 +0530

    added new package and class

---
+++ src/test/java/contacts/CreateContactsTest.java
@@ -0,0 +1,14 @@
+package contacts;
+
+import org.testng.annotations.Test;
+
+public class CreateContactsTest {
+	@Test
+	public void create() {
+		int a=10;
+		int b=30;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+
+}
